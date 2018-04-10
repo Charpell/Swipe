8e7833610c9c8ecd3fdb0dccf506d66ac5237420
Charpell <charpellumeh@gmail.com>
2018-04-10 05:00:58 +0100

    Advancing the deck

--- src/Deck.js
+++ src/Deck.js
@@ -50,6 +50,7 @@ class Deck extends Component {
 
     direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item)
     this.state.position.setValue({ x: 0, y: 0 })
+    
     this.setState({ index: this.state.index + 1 })
   }
   
@@ -76,8 +77,12 @@ class Deck extends Component {
   }
 
   renderCards() {
-    return this.props.data.map((item, index)=> {
-      if (index === 0) {
+    return this.props.data.map((item, i)=> {
+      if (i < this.state.index) {
+        return null
+      }
+
+      if (i === this.state.index) {
         return (
           <Animated.View
            key={item.id} 
